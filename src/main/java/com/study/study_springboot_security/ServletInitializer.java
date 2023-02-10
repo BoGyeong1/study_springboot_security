c959ba0af9ca5bb67614c65ad30a0e41851e9ff6
BoGyeong1 <wls6590@gmail.com>
2023-02-10 19:19:36 +0900

    Up

--- src/main/java/com/study/study_springboot_security/ServletInitializer.java
+++ src/main/java/com/study/study_springboot_security/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.study.study_springboot_security;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+ 
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
